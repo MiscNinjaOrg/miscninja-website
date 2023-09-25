8f67fc03b2b820e4247a6f538e52f0c96ef862dd
nalin <stackoverflowpanic@gmail.com>
2023-09-25 18:47:10 -0400

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,17 +15,18 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and Open-Source Next.js Template for Startup & SaaS
+                Open-Source Redis Extension to Supercharge Your AI Workflow
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Startup is free Next.js template for startups and SaaS business websites comes with all the essential pages, components, and sections you need to launch a complete business website, built-with Next 13.x and Tailwind CSS.
+                MiscNinja is free Redis extension for AI power-users, developers and businesses.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                   🔥 Get Pro
+                   <del>Get Pro</del> <br></br>
+                   Become an AI Ninja
                   </Link>
                   <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"
